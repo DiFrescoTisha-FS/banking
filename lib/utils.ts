d6f92f9a272f26481a245c6e51645423d9f4005f
Tisha Di Fresco <tbdifresco@student.fullsail.edu>
2024-08-08 16:28:27 -0400

    Implement Auth, appwrite, sentry

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -193,3 +194,18 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authFormSchema = (type: string) => z.object({
+  // sign up
+  firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3).optional(),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3).optional(),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().max(50).optional(),
+  city: type === 'sign-in' ? z.string().optional() : z.string().max(50).optional(),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2).optional(),
+  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6).optional(),
+  dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3).optional(),
+  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3).optional(),
+  // both
+  email: z.string().email(),
+  password: z.string().min(8),
+});
